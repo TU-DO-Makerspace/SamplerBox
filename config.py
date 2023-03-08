729a59e385d7acf6c9931852d93f434bd7de2390
CTXz <ctx.xda@gmail.com>
2023-03-08 19:43:05 +0100

    config.py: Use sysdefault audio device
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- config.py
+++ config.py
@@ -3,8 +3,13 @@
 # CONFIG
 #########################################
 
-AUDIO_DEVICE_ID = 2                     # change this number to use another soundcard
-SAMPLES_DIR = "/media/"                 # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
+AUDIO_DEVICE_ID = 1                     # The ID of the audio device to use. 
+					# 1 = Systems default device which will automatically switch to the USB audio device when connected
+					# See https://python-sounddevice.readthedocs.io/en/0.4.6/api/checking-hardware.html
+
+SAMPLES_DIR = "/media/"                 # The root directory containing the sample-sets.
+					# Example: "/media/" to look for samples on a USB stick / SD card
+					
 IGNORE_MIDI_AFTER_BOOT_FOR_SECONDS = 2  # Discard MIDI messages for provided seconds after boot to work-around garbage MIDI messages on boot
 MAX_POLYPHONY = 80                      # This can be set higher, but 80 is a safe value
 USE_BUTTONS = False                     # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset
