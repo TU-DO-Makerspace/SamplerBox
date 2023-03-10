9a484ad63aeb8014f3434d237b9944fe93aef63b
Patrick Pedersen <ctx.xda@gmail.com>
2023-03-10 06:35:21 +0100

    Display Server: Overhaul to support characters
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- display_server.py
+++ display_server.py
@@ -23,32 +23,62 @@ class DisplayServer(object):
 
 		self._initialized = True
 
+	def _num_val_to_2c(self, val: int):
+		if val < 10:
+			return '0' + str(val)
+		elif val > 99:
+			return 'XX'
+		else:
+			return str(val)
+
+	def _str_val_to_2c(self, val: str):
+		if len(val) == 1:
+			return val + ' '
+		elif len(val) == 2:
+			return val
+		else:
+			return 'XX'
+
 	def initialized(self):
 		return self._initialized
 	
-	def set_layer1(self, val):
+	def set_layer1_n(self, val: int):
 		if not self._initialized:
 			return
-		
-		self._layer1_val = val
+		self._layer1_val = self._num_val_to_2c(val)
 
-	def set_layer2(self, val, duration_s):
+	def set_layer1_2c(self, s: str):
 		if not self._initialized:
 			return
+		self._layer1_val = self._str_val_to_2c(s)
 
-		self._layer2_val = val
+	def set_layer2_n(self, val: int, duration_s: int):
+		if not self._initialized:
+			return
+		self._layer2_val = self._num_val_to_2c(val)
+		self._layer2_tstamp = time.time() + duration_s
+
+	def set_layer2_2c(self, s: str, duration_s: int):
+		if not self._initialized:
+			return
+		self._layer2_val = self._str_val_to_2c(s)
 		self._layer2_tstamp = time.time() + duration_s
 
 	def _display(self, val):
 		if val == None:
 			self._seg1.off()
 			self._seg2.off()
-		elif val < 10:
-			self._seg1.value = '0'
-			self._seg2.value = str(val)
+			return
+		
+		if val[0] == ' ':
+			self._seg1.off()
 		else:
-			self._seg1.value = str(val)[0]
-			self._seg2.value = str(val)[1]
+			self._seg1.value = val[0]
+		
+		if val[1] == ' ':
+			self._seg2.off()
+		else:
+			self._seg2.value = val[1]
 
 	def _update(self):
 		if self._layer2_tstamp != None:
