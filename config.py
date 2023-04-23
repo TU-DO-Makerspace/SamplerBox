5f3600c2ea0259730cb9cde523b418585dd7637a
CTXz <ctx.xda@gmail.com>
2023-04-23 13:44:30 +0200

    config.py: Use RPI config for this branch, not the PC config
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- config.py
+++ config.py
@@ -3,27 +3,27 @@
 # CONFIG
 #########################################
 
-TARGET_PLATFORM = "LINUX"                   # The target device to run on. Possible values: "RPI", "LINUX"
-AUDIO_DEVICE_ID = 7                     # The ID of the audio device to use. 
-					# 1 = Systems default device which will automatically switch to the USB audio device when connected
-					# See https://python-sounddevice.readthedocs.io/en/0.4.6/api/checking-hardware.html
+TARGET_PLATFORM = "RPI"                   # The target device to run on. Possible values: "RPI", "LINUX"
+AUDIO_DEVICE_ID = 1                       # The ID of the audio device to use. 
+										  # 1 = Systems default device which will automatically switch to the USB audio device when connected
+										  # See https://python-sounddevice.readthedocs.io/en/0.4.6/api/checking-hardware.html
 
-SAMPLES_DIR = "/media/patrick/SAMPLES/"                 # The root directory containing the sample-sets.
-					# Example: "/media/" to look for samples on a USB stick / SD card
+SAMPLES_DIR = "/media/"					  # The root directory containing the sample-sets.
+										  # Example: "/media/" to look for samples on a USB stick / SD card
 					
-IGNORE_MIDI_AFTER_BOOT_FOR_SECONDS = 2  # Discard MIDI messages for provided seconds after boot to work-around garbage MIDI messages on boot
-MAX_POLYPHONY = 80                      # This can be set higher, but 80 is a safe value
-MAX_PRESETS = 99                        # The maximum number of presets to load (99 as the 7-segment display only has 2 digits)
-USE_BUTTONS = False                     # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset
-BUTTON_PREV = 18                        # The GPIO pin to use for the previous preset button
-BUTTON_NEXT = 17                        # The GPIO pin to use for the next preset button
-USE_DOUBLE_7SEGMENT_DISPLAY = False     # Set to True to use a double 7-segment display (connected to RaspberryPi's GPIO pins)
-USE_SERIALPORT_MIDI = False             # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
-USE_MUTE_LED = False                    # Set to True to use a LED to indicate mute status (connected to RaspberryPi's GPIO pins)
-GPIO_MUTE_LED = 17                      # The GPIO pin to use for the mute LED
-USE_SYSTEMLED = False                   # Flashing LED after successful boot, only works on RPi/Linux
-USE_3_MIDICHANNELS = True		# Setting true allows to load 3 different sample sets on 3 different MIDI channels
-					# We're using three as this option was tailored for the Arturia Beatstep Pro, which
-					# can independently sequence 3 MIDI channels.
-MIDI_CH_SELECT_SWITCH_LEFT = 23		# MIDI Channel 3-way switch left pin
-MIDI_CH_SELECT_SWITCH_RIGHT = 24	# MIDI Channel 3-way switch right pin
+IGNORE_MIDI_AFTER_BOOT_FOR_SECONDS = 2    # Discard MIDI messages for provided seconds after boot to work-around garbage MIDI messages on boot
+MAX_POLYPHONY = 80                        # This can be set higher, but 80 is a safe value
+MAX_PRESETS = 99                          # The maximum number of presets to load (99 as the 7-segment display only has 2 digits)
+USE_BUTTONS = True                        # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset
+BUTTON_PREV = 18                          # The GPIO pin to use for the previous preset button
+BUTTON_NEXT = 17                          # The GPIO pin to use for the next preset button
+USE_DOUBLE_7SEGMENT_DISPLAY = True        # Set to True to use a double 7-segment display (connected to RaspberryPi's GPIO pins)
+USE_SERIALPORT_MIDI = True                # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
+USE_MUTE_LED = False                      # Set to True to use a LED to indicate mute status (connected to RaspberryPi's GPIO pins)
+GPIO_MUTE_LED = 00                        # The GPIO pin to use for the mute LED
+USE_SYSTEMLED = True                      # Flashing LED after successful boot, only works on RPi/Linux
+USE_3_MIDICHANNELS = True				  # Setting true allows to load 3 different sample sets on 3 different MIDI channels
+										  # We're using three as this option was tailored for the Arturia Beatstep Pro, which
+										  # can independently sequence 3 MIDI channels.
+MIDI_CH_SELECT_SWITCH_LEFT = 23			  # MIDI Channel 3-way switch left pin
+MIDI_CH_SELECT_SWITCH_RIGHT = 24		  # MIDI Channel 3-way switch right pin
